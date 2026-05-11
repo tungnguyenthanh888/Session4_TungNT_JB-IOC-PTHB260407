5cc5ff60823c08208393f233ad6cb1d6b92058df
tungnguyenthanh888 <tungdev3010@gmail.com>
2026-05-11 17:03:24 +0700

    "Add service into eureka server"

---
+++ customer-service/src/test/java/com/session4/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.session4.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ product-service/src/test/java/com/session4/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.session4.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
