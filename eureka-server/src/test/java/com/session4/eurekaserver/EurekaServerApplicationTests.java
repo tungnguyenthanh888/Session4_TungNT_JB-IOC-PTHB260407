fea3932548d2c51e2754280dfe5b8ef3d9142b16
tungnguyenthanh888 <tungdev3010@gmail.com>
2026-05-11 16:51:40 +0700

    Eureka server configuration

---
+++ eureka-server/src/test/java/com/session4/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.session4.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
